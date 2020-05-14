eae2f32b042d00dbae7dcefd0a1bf8e2fe7bf9db
djadsaunders <djadsaunders@gmail.com>
2020-05-14 13:13:18 +0100

    Parameterise the server IP

--- cabin-temp.py
+++ cabin-temp.py
@@ -1,12 +1,16 @@
+import sys
 import Adafruit_DHT
 import paho.mqtt.client as mqtt
 from time import sleep
 
 sensor = Adafruit_DHT.DHT22
+serverIP = sys.argv[1]
+
+print("Sending signals to " + serverIP)
 
 # Connect to broker
 mqttClient = mqtt.Client("djad_mqtt")
-mqttClient.connect("192.168.0.101", 1883)
+mqttClient.connect(serverIP, 1883)
 mqttClient.loop_start()
 
 pin = 4
@@ -18,8 +22,8 @@ try:
         sleep(2.5)
         if humidity is not None and temperature is not None:
             print("Temp={0:0.1f}*C Humidity={1:0.1f}%".format(temperature, humidity))
-        	mqttClient.publish("cabin/temp", temperature)
-        	mqttClient.publish("cabin/humidity", humidity)
+            mqttClient.publish("cabin/temp", temperature)
+            mqttClient.publish("cabin/humidity", humidity)
         else:
             print("Failed to get reading.")
 
